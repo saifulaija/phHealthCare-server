977bf0e6f266ed1046436ac55b45bec1ac41c3d1
saifulaija <saifulaija2023@gmail.com>
2024-04-24 20:01:06 +0600

    all api done

---
+++ src/app/modules/schedule/schedule.interface.ts
@@ -0,0 +1,24 @@
+// export type ISchedule = {
+//   startDate: string;
+//   endDate: string;
+//   startTime: string;
+//   endTime: string;
+// };
+
+// export type IScheduleFilterRequest = {
+//   startDate?: string | undefined;
+//   endDate?: string | undefined;
+// };
+
+
+export type ISchedule = {
+  startDate: string;
+  endDate: string;
+  startTime: string;
+  endTime: string;
+};
+
+export type IScheduleFilterRequest = {
+  startDate?: string | undefined;
+  endDate?: string | undefined;
+};
